40c38d4df23aa342f89c07d84bd7c734f751003d
Denise Henderson <denise@henderson3.us>
2020-09-25 14:59:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Denise"
+  end
+
+  get '/hometown' do
+    "My hometown is Jacksonville"
+  end
 end
